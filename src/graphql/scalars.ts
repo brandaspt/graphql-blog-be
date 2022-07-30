fbb9deb76545c96f617339d3e885cb4ca17cf129
Tiago Brandao <tiagofpbrandao@gmail.com>
2022-07-31 00:29:04 +0200

    feat: add email scalar

--- src/graphql/scalars.ts
+++ src/graphql/scalars.ts
@@ -1,4 +1,5 @@
-import { DateTimeResolver } from "graphql-scalars"
+import { DateTimeResolver, EmailAddressResolver } from "graphql-scalars"
 import { asNexusMethod } from "nexus"
 
 export const dateTimeScalar = asNexusMethod(DateTimeResolver, "date")
+export const emailScalar = asNexusMethod(EmailAddressResolver, "email")
